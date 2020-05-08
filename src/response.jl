aa24ef15e86b97d7519bf805c5ae3ebaf75f5378
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-05-08 14:38:49 -0400

    cleanup strongs

--- src/response.jl
+++ src/response.jl
@@ -26,6 +26,20 @@ function Base.replace!(r::Response, rep::Pair)
     return r
 end
 
+_isempty(x::HTMLText) = isempty(x.text)
+_isempty(x::HTMLElement) = isempty(x.children) || all(isempty(child) for child in x.children)
+
+function cleanup_strong!(elem::HTMLElement)
+    if length(elem.children) == 1 && first(elem.children) isa HTMLElement{:strong}
+        elem.children = [elem.children[1].children[1]]
+    else
+        filter!(x -> !(isa(x, HTMLElement{:strong}) && _isempty(x)), elem.children)
+    end
+    return elem
+end
+
+cleanup_strong!(elem::HTMLText) = HTMLText
+
 function response(elem::HTMLElement, label, correct=false)
     elem = deepcopy(elem)
     for node in PostOrderDFS(elem)
@@ -35,12 +49,7 @@ function response(elem::HTMLElement, label, correct=false)
     end
 
     # re-parse to get rid of any emtpy elements.  children[2] is <body>, children[1] is body contents
-    elem = parsehtml(string(elem)).root.children[2].children[1]
-    
-    if length(elem.children) == 1 && first(elem.children) isa HTMLElement{:strong}
-        elem.children = [elem.children[1].children[1]]
-    end
-
+    elem = cleanup_strong!(parsehtml(string(elem)).root.children[2].children[1])
     Response(elem, label, string(uuid4()), correct)
 end
 
